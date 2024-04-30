0414f1d6a5d2b0b9eabd1f8d3170b0c28070f157
Umut Sevdi <sevdiumut@pm.me>
2024-05-01 02:03:48 +0300

    updated for test

--- server.py
+++ server.py
@@ -8,6 +8,9 @@
 #*****************************************************************************
 import argparse
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+from datetime import datetime
+import time
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -24,10 +27,18 @@ def run(port=8000, server_class=HTTPServer, handler_class=SimpleHTTPRequestHandl
     print(f"Echo server on port:{port}")
     httpd.serve_forever()
 
+def print_datetime():
+    while True:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print("Current date and time:", current_time)
+        time.sleep(10)
 
 parser = argparse.ArgumentParser(description='A demo echo server')
 parser.add_argument('-p', '--port', type=int, default=8000, help='Port number (default: 8000)')
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    datetime_thread = threading.Thread(target=print_datetime)
+    datetime_thread.daemon = True  # Daemonize the thread so it doesn't block program exit
+    datetime_thread.start()
     run(args.port)
